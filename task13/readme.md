de8511f8c30ca173fd610a53ca9a9c04dc4206b9
henik19 <rangoonwalahenik19@gmail.com>
2026-03-16 15:50:38 +0530

    Task13 added

---
+++ task13/readme.md
@@ -0,0 +1,17 @@
+# Odd even
+
+## Formula Used
+n % 2 == 0
+
+Where:
+n= number
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## How to Run
+1. Open the folder.
+2. Open index.html in the browser.
+3. Check the console for output.
---
+++ task13/script.js
@@ -0,0 +1,9 @@
+var n = 2;
+
+if (n % 2 == 0) {
+    console.log("Number " + n + " is even");
+    document.getElementById("result").innerHTML = "The Number " + n + " is even";
+}else {
+    console.log("Number " + n + " is odd");
+    document.getElementById("result").innerHTML = "The Number " + n + " is odd";
+}
