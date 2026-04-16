bd5b623b967d70051580525dffcc49b445cdd64b
henik19 <rangoonwalahenik19@gmail.com>
2026-04-16 12:16:15 +0530

    task26 added

--- task26/vite.config.js
+++ task26/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mern/task26/',   // 👈 THIS FIXES YOUR ERROR
+  base: '/mern/task26/',   
 })
